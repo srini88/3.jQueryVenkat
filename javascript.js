00158525d309746703eb6fb6b6522ddbd5a0994f
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 18:19:48 -0500

    more

--- javascript.js
+++ javascript.js
@@ -31,6 +31,14 @@
 
 jQuery(document).ready(function(){
 
+	console.log($("#btn"));   //[input#btn, context: document, selector: "#btn"]
+    console.log(typeof $("#btn"));   //object
+	//indexing with 0 - to get the raw javascript
+	console.log($("#btn")[0]);  //raw object <input type="button" id="btn" value="clickme"></input>
+
+	console.log($("#btn").css("font-size"));  //13.333 px getting here
+	console.log(typeof $("#btn").css("font-size"));   //returns string 
+
 	$("#btn").css({
 		"backgroundColor": 'red',
 		"font-size": "200%"
@@ -52,3 +60,16 @@ jQuery(document).ready(function(){
 
 // Example
 // $("p").css("background-color");
+
+
+
+
+
+/////javascript document.getElemetByID and jQuery id selector are not the same
+
+////document.getElementById returns a raw DOM object where as jquery id selector returns a jquery object that wraps the DOM object and provides jquery methods...
+
+///to get the underlying DOM oject from a jquery object use $('#id')[0]
+
+
+// raw javascript is slgithly faster than jquery id selector
