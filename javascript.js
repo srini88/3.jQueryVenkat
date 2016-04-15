f5e7b396e09258b35f6b807eceb46eaa82be9648
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 17:49:48 -0500

    fall back from CDN gracefully

--- javascript.js
+++ javascript.js
@@ -1,14 +1,23 @@
+///benefits of CDn
+
+/////////////Content delivery network...systemo of distributed servers that hosts resources as images, CSS< JS files etc
+
+// instead of hosting on own web server 
+
+////jquery will be download from the CDN server that is closest to the user..
+
+////browser caching -- jquery is used on many popular websites...if a user has already visited a webpage that uses jquery from CDN, the jquery file would already been cached..
+
+///parallel download..
+
+///reduced server load --- the http request for jquery file is handled by the cdn server, so the load on your web server is reduced.... this also means there is a saving on your website bandwidth consumption which in turn will reduce your hosting cost.
 
 
-///with document.ready height and width are set to zero///getting some weird answers 
+////your client firewalls manay block the CDN. so you may have to request your clients to whitelist the CDN
 
-//by the document.ready event is fired.... the img is not laded...
-////thats why it reads the height and width as zero
+////what if the requrie jqery file from CDN server is not downloaded...
 
-
-$(document).ready(function (){
-	///get the div1
-	//// and set the html of the div...
-	$("#div1").html("Height :" + $('#Image1').height() + "<br/>" + "Width: " + $('#Image1').width())
-});
-
+///so you got to fall back gracefully
+///backslash in the closing script tag --- to escape the /
+//the below check if the jQuery has been loaded from the CDN , if not, do the document.write 
+window.jQuery || document.write("<script src='myJqeury.js'><\/script");
