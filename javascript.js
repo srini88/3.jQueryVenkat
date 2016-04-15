6f827be7278e454df626f00dcf76d2fb8fc3af47
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 17:36:45 -0500

    getimgwidth and hiegit

--- javascript.js
+++ javascript.js
@@ -1,20 +1,32 @@
-///window loaded 
+/////ready probalby the most used 
+
+///to get the actual image dimensitons, we will have to wait till the image is fully loaded....
+///so use window.load 
+
+
+
+// $(window).load  -- in jquery 
+// window.onload in traditional JS
+////you want to get actual image height and width, have to wait until the image is fully loaded 
+///you got a funking div 
 
 
 $(window).load(function (){
-	console.log("window loaded")
+	///get the div1
+	//// and set the html of the div...
+	$("#div1").html("Height :" + $('#Image1').height() + "<br/>" + "Width: " + $('#Image1').width())
 });
 
 
-////ready fires before window.load 
-$(document).ready(function (){
-	console.log("documnet DOM loaded")
-});
+// The html() method sets or returns the content (innerHTML) of the selected elements.
 
+// When this method is used to return content, it returns the content of the FIRST matched element.
 
-//op
+// When this method is used to set content, it overwrites the content of ALL matched elements.
 
-// documnet DOM loaded  - this prints first 
-// window loaded
+// Return content:
 
+// $(selector).html()
+// Set content:
 
+// $(selector).html(content)
