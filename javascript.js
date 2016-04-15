48b8725ddc0f0ed9927bae8852e59279a8c38af9
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 18:34:34 -0500

    tr td

--- javascript.js
+++ javascript.js
@@ -1,75 +1,47 @@
-///id selector 
+// $('td') - select all the td elements 
 
-//jqeury selectors
+// $('div a'); - div is ancestor - anchor is descendent - give us all anchor elemnts that are inside a div element 
 
-///alllow you to select and manipulate HTML elements
 
-////elemt ID 
-
-///e;emtn tag name
-
-//element class name
-//element attribute
-///element attribute values and many more 
+// $('div, span, a'); //return all div span and anchor elents 
 
 
 
-// # symbol witin jquery is the ID selector 
 
-// you are attaching clcik event handler to the button that is returned. 
+$(document).ready(function(){
+
+	// <tr>
+ //            <td>C#</td>
+ //            <td>ASP.NET</td>
+ //            <td>SQL Server</td>
+ //        </tr>
+
+    $('tr td').html("nigga");   //picks all trs and tds within that tr
+
+    console.log($('tr td'));   //gives all the 15 td elements 
+
+    console.log($('tr td').length);  //find all td elemtns  - length = 15
 
 
+    ///chnage the background of each of tr to red
 
-// ///change the background clor of the method
+    $('tr').css({
+    	'backgroundColor': 'red'
+    });
+      //////changed
 
-// ///use the method css  - that has name and value 
 
-// <input type="button" id="btn" value="clickme"></input>
+    console.log($('table').html());   //entire html content of table elemnt 
 
 
-////no dot after jqeury or $
+    //lets alert each table row 
+    //loop through each tr element
+   
+    // 	//this references the current tr element, we want to get html of the current table row -
+    
+    $("table tr").each(function (){
+    	alert($(this).html());
+    });
+    	
 
-jQuery(document).ready(function(){
-
-	console.log($("#btn"));   //[input#btn, context: document, selector: "#btn"]
-    console.log(typeof $("#btn"));   //object
-	//indexing with 0 - to get the raw javascript
-	console.log($("#btn")[0]);  //raw object <input type="button" id="btn" value="clickme"></input>
-
-	console.log($("#btn").css("font-size"));  //13.333 px getting here
-	console.log(typeof $("#btn").css("font-size"));   //returns string 
-
-	$("#btn").css({
-		"backgroundColor": 'red',
-		"font-size": "200%"
-		
-	});
 });
-
-
-// Set Multiple CSS Properties
-// To set multiple CSS properties, use the following syntax:
-
-// css({"propertyname":"value","propertyname":"value",...});
-
-
-// To return the value of a specified CSS property, use the following syntax:
-
-// css("propertyname");
-// The following example will return the background-color value of the FIRST matched element:
-
-// Example
-// $("p").css("background-color");
-
-
-
-
-
-/////javascript document.getElemetByID and jQuery id selector are not the same
-
-////document.getElementById returns a raw DOM object where as jquery id selector returns a jquery object that wraps the DOM object and provides jquery methods...
-
-///to get the underlying DOM oject from a jquery object use $('#id')[0]
-
-
-// raw javascript is slgithly faster than jquery id selector
