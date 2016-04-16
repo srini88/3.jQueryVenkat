8392df43b402d7b7566d77da54ba02437f0f397e
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 19:03:37 -0500

    mores hit

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@
 //jquery class selectors - getElemtnByClas s behind scenes 
 
 
+//span elements with class big ... period because class 
+$('span.big')  //tag names just use like that
 
 
 $('.class')
@@ -20,12 +22,21 @@ $('.small, .big') //all ements with class small or big
 $('div.small, .big')  //all div elemtns with class small and any element with class big
 
 
+//sleect all eleemtns with class small that are nested in an elemetn with id = div2
+
+//ancestor and descendent syntax -- always have space 
+$("#div2 .small")
+
 
 
 $(document).ready(function(){
 
-	$("div.small, .big").css({
-		backgroundColor: 'red'
+	$("div.small").css({
+		border : '5px solid red'
 	});
+	// or   no difference -- border without quotations
+	// $("div.small, .big").css({
+	// 	'border' : '5px solid red'
+	// });
 
 });
