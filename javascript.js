a85ee56831cee4cea86dec506d5b29423c799eb9
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 19:12:07 -0500

    ancestor desc

--- javascript.js
+++ javascript.js
@@ -2,8 +2,12 @@
 
 
 $(document).ready(function(){
-	//div that contains both the classes small and big
-	$(".small.big").css({
+	//if we have a space etwen class names 
+
+//fuind all the elements that have class .big that are nested inside class .small
+//you are doing ancestor descendent syntasx  - no border around these elemtns 
+	$(".small .big").css({
 		border : '5px solid red'
 	});
 });
+
