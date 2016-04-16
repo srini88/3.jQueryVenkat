674389ad3e1731fdc1f092a31a80d9ed450ec6c5
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 19:15:24 -0500

    filter

--- javascript.js
+++ javascript.js
@@ -4,10 +4,13 @@
 $(document).ready(function(){
 	//if we have a space etwen class names 
 
-//fuind all the elements that have class .big that are nested inside class .small
-//you are doing ancestor descendent syntasx  - no border around these elemtns 
-	$(".small .big").css({
-		border : '5px solid red'
-	});
+// find alll elements with class small  and filter the ones that have .big
+	$(".small").filter('.big').css({border : '5px solid red'});
 });
+
+
+///using filter method 
+
+
+
 
