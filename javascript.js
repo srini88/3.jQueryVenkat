3f3a92cd4948968fa27092f15a73d0c1bcaf1b4d
srini88 <pvsrinivas.ece@gmail.com>
2016-04-15 19:09:21 -0500

    class = "small big">

--- javascript.js
+++ javascript.js
@@ -1,37 +1,9 @@
-// $('td') - select all the td elements 
-
-// $('div a'); - div is ancestor - anchor is descendent - give us all anchor elemnts that are inside a div element 
-
-
-// $('div, span, a'); //return all div span and anchor elents 
-
-
-
-//jquery class selectors - getElemtnByClas s behind scenes 
-
-
-//span elements with class big ... period because class 
-$('span.big')  //tag names just use like that
-
-
-$('.class')
-
-
-$('.small') //select all ements with class small 
-$('.small, .big') //all ements with class small or big
-$('div.small, .big')  //all div elemtns with class small and any element with class big
-
-
-//sleect all eleemtns with class small that are nested in an elemetn with id = div2
-
-//ancestor and descendent syntax -- always have space 
-$("#div2 .small")
 
 
 
 $(document).ready(function(){
-	//ancestor and descendent - needs a space 
-	$("#div2 .small").css({
+	//will apply to both small big and small - becuase both have small
+	$(".small").css({
 		border : '5px solid red'
 	});
 });
